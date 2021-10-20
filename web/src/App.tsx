5d73e699541bb12dd57f8f62b0b8756d0e36039b
Andrelino Silva <andrelinocom@gmail.com>
2021-10-20 07:08:30 -0300

    Create: Project web,add sass, components, :rocket:

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import {MessageList} from './components/MessageList'
+
+import styles from './App.module.scss'
+import { LoginBox } from './components/LoginBox'
+
+export function App() {
+
+  return (
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
+  )
+}
+
+
