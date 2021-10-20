0d54adfdff21c2ed03d1879ead1170ea7246f7a7
Andrelino Silva <andrelinocom@gmail.com>
2021-10-20 15:18:06 -0300

    End: Terminado o back

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,69 @@
-export function MessageList(){
-  return (
-    <h1>MessageList</h1>
-  )
+import styles from './styles.module.scss';
+import { api } from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+import { useEffect } from 'react';
+
+export function MessageList() {
+
+  useEffect(() => {
+    api.get('messages').then((response) => {
+      console.log(response.data)
+    })
+  },[])
+
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContainer}>
+                        lorem ipsum dolor sit amet, consect lorem ipsum dolor
+                        sit amet
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img
+                                src="https://github.com/andrelinos.png"
+                                alt="Andrelino Silva"
+                            />
+                        </div>
+                        <span>Andrelino Silva</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContainer}>
+                        lorem ipsum dolor sit amet, consect lorem ipsum dolor
+                        sit amet
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img
+                                src="https://github.com/andrelinos.png"
+                                alt="Andrelino Silva"
+                            />
+                        </div>
+                        <span>Andrelino Silva</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContainer}>
+                        lorem ipsum dolor sit amet, consect lorem ipsum dolor
+                        sit amet
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img
+                                src="https://github.com/andrelinos.png"
+                                alt="Andrelino Silva"
+                            />
+                        </div>
+                        <span>Andrelino Silva</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
 }
